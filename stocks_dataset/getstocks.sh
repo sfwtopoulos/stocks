0e968ddcf298539a5966a8c3d9be1813461166e2
sfwtopoulos <61181262+sfwtopoulos@users.noreply.github.com>
2020-02-18 11:41:49 +0200

    Update getstocks.sh

--- stocks_dataset/getstocks.sh
+++ stocks_dataset/getstocks.sh
@@ -6,3 +6,13 @@ while read l; do
 	echo $l
 	curl https://www.nasdaq.com/api/v1/historical/$l/stocks/2010-01-01/2020-01-01 > ./csv_data/$l.csv
 done < sp100.list
+
+for f in *.csv
+do
+ sed 's/$/'," ""$f"'/' $f
+done > combined
+
+rm ./*.csv
+
+sed -i 's/.csv//' combined
+mv combined combined.csv
